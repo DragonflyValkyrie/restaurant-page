6bd8e4df24b0ce8d47fc9e119a94d5b89eb291ee
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2023-12-20 23:47:36 -0500

    Added contact content page with styling and form

--- src/about.js
+++ src/about.js
@@ -22,19 +22,19 @@ const aboutPage = (contentContainer) => {
 
     // Create individual paragraphs and set their content
     const p1 = document.createElement("p");
-    p1.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...";
+    p1.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec scelerisque sem, quis ultricies lorem. Sed sollicitudin, ipsum eget viverra laoreet, orci tellus blandit sem, nec facilisis mi dui at risus. Fusce volutpat ornare semper. Aenean egestas efficitur sem, ut ullamcorper eros vestibulum a. Aliquam at lacus sit amet nisi egestas hendrerit ut vel enim. Donec semper dolor sapien, sit amet feugiat massa egestas quis. Vivamus justo purus, imperdiet non urna sit amet, eleifend consectetur odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed neque odio. Praesent luctus pulvinar ligula vel porta. Maecenas vel volutpat nibh. Nullam auctor in elit non malesuada.";
 
     const p2 = document.createElement("p");
-    p2.textContent = "Nam rutrum varius tincidunt. ...";
+    p2.textContent = "Nam rutrum varius tincidunt. Donec dapibus mauris nisl, dictum mattis orci finibus eget. Nullam a pellentesque augue. Vivamus feugiat augue tellus, vitae rhoncus quam interdum interdum. Nunc imperdiet finibus volutpat. In efficitur vehicula ex, sit amet accumsan quam fermentum quis. Phasellus leo ante, elementum a nisl vitae, vulputate porttitor tortor. In sed tincidunt tellus.";
 
     const p3 = document.createElement("p");
-    p3.textContent = "Integer non enim turpis. ...";
+    p3.textContent = "Integer non enim turpis. In non efficitur nunc, at efficitur magna. Donec ut justo auctor, venenatis urna ac, luctus tortor. Nullam vel porttitor arcu, euismod sagittis mauris. Fusce vehicula sem lorem, at mattis massa vestibulum at. Etiam diam elit, auctor non lobortis ut, vehicula eu erat. Suspendisse potenti. Nam laoreet risus eu metus varius luctus. Nunc vestibulum blandit ante, in rhoncus erat maximus ut. Quisque egestas, mauris elementum luctus vestibulum, odio metus auctor arcu, nec ornare lorem odio ac turpis. Pellentesque eleifend pellentesque libero et efficitur. Cras vel faucibus augue, non tempor metus. Nunc tincidunt, metus ut vulputate venenatis, dui tellus faucibus odio, at fringilla est quam nec nisl. Mauris tortor ipsum, vulputate quis sem a, blandit porta est.";
 
     const p4 = document.createElement("p");
-    p4.textContent = "Praesent vel lobortis velit. ...";
+    p4.textContent = "Praesent vel lobortis velit. Mauris lobortis blandit libero, eget facilisis diam tempor et. Nulla non ultricies purus, in sollicitudin leo. Vestibulum non augue nec urna varius commodo eu a tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget consectetur eros. Phasellus a arcu nisl. Donec euismod maximus purus, sit amet maximus erat.";
 
     const p5 = document.createElement("p");
-    p5.textContent = "Sed interdum lorem maximus vehicula ultrices. ...";
+    p5.textContent = "Sed interdum lorem maximus vehicula ultrices. Nam nisl ligula, faucibus sit amet bibendum semper, tempor eget nisl. Phasellus dui urna, posuere ac maximus vitae, elementum in leo. Phasellus posuere convallis mauris sit amet varius. Fusce scelerisque sem varius nulla porttitor, eget aliquam felis mattis. Ut eu vulputate dolor, nec blandit enim. Suspendisse blandit est et faucibus vulputate.";
 
     // Append titles to their container
     aboutTitleContainer.appendChild(aboutTitle);
--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,59 @@
 const contactPage = (contentContainer) => {
 
-  
-    contentContainer.appendChild(contact);
-  };
-  
-  export { contactPage };
+    // Create the main container for the contact page
+    const contactContainer = document.createElement("div");
+    contactContainer.classList.add("contact-container");
+
+    // Create container for the title
+    const contactTitleContainer = document.createElement("div");
+    contactTitleContainer.classList.add("contact-titles");
+
+    // Create and set the main title for the contact page
+    const contactTitle = document.createElement("h1");
+    contactTitle.textContent = "Contact Us";
+   
+    // Create container for the paragraphs of the contact content
+    const parContactContainer = document.createElement("div");
+    parContactContainer.classList.add("contact-para");
+
+    // Create individual paragraphs and set their content
+    const p1 = document.createElement("p");
+    p1.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum lectus sit amet nulla ornare porta. Aliquam urna mi, condimentum quis dapibus ac, luctus quis urna. Phasellus nulla nisl, mollis ut nisl a, aliquam sodales neque. Phasellus aliquet pellentesque vehicula. Phasellus sit amet leo ac dolor efficitur tempor id ut risus. In ut lectus at est vestibulum mollis volutpat ut eros. Sed ac ipsum nec purus vulputate efficitur vitae pellentesque mi. Mauris tincidunt ac arcu eget lobortis. Duis condimentum sapien non felis tristique, eu rhoncus mauris mollis.";
+
+    const p2 = document.createElement("p");
+    p2.textContent = "Pellentesque sit amet blandit libero, consequat auctor elit. Proin maximus lectus id metus vestibulum, at elementum magna ullamcorper. Phasellus non elit nibh. Mauris gravida libero at enim rhoncus, et pretium justo posuere. Donec est sapien, suscipit eu egestas vel, mattis at leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin fermentum eros sapien, eget sollicitudin metus vulputate vel. Integer vulputate a purus non pulvinar. Nulla nec blandit tortor. Cras commodo et ex non rutrum. Fusce tempor maximus sodales. Mauris nulla libero, consequat sit amet lobortis a, vehicula at mi. In posuere, metus eget semper ullamcorper, lacus odio scelerisque lacus, nec laoreet ex justo ac lectus. Etiam a turpis ex. Quisque nisi urna, egestas cursus mi quis, viverra luctus ex.";
+
+    // Append paragraphs to the contact content container
+    parContactContainer.appendChild(p1);
+    parContactContainer.appendChild(p2);
+
+    // Create the form element
+    const contactForm = document.createElement("form");
+
+    // Create input fields, labels, and other form elements here as needed
+    const nameLabel = document.createElement("label");
+    nameLabel.textContent = "Name:";
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+
+    const emailLabel = document.createElement("label");
+    emailLabel.textContent = "Email:";
+    const emailInput = document.createElement("input");
+    emailInput.type = "email";
+
+    // Append the form elements to the form
+    contactForm.appendChild(nameLabel);
+    contactForm.appendChild(nameInput);
+    contactForm.appendChild(emailLabel);
+    contactForm.appendChild(emailInput);
+
+    // Append title, contact content container, and form to the main contact container
+    contactContainer.appendChild(contactTitle);
+    contactContainer.appendChild(parContactContainer);
+    contactContainer.appendChild(contactForm);
+
+    // Append the main contact container to the content container of the page
+    contentContainer.appendChild(contactContainer);
+};
+
+export { contactPage };
